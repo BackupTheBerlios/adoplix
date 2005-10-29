d8069d1e38b10bad8f0392b3928fda9a48a2d667
dirkg <dirkg>
2005-10-29 22:45:08 +0000

    Beginn einer server-ähnlichen Implementierung

--- src/de/adoplix/internal/server/adoplixServer.java
+++ src/de/adoplix/internal/server/adoplixServer.java
@@ -1,37 +1,80 @@
 package de.adoplix.internal.server;
 
+import de.adoplix.internal.tasks.TaskConfiguration;
+
 /**
  *
  * @author dirk
  */
 public class AdoplixServer {
+	
+	/** Absolute path of Configuration-File */
+	private static String _pathConfiguration = "";
+	/** Container of own configuration */
+	private ServerConfiguration _serverConfiguration = null;
+	/** Container of task configuration */
+	private TaskConfiguration _taskConfiguration = null;
     
     public static void main(String[] args) {
-        try {
-            String fileName = "C:/Dokumente und Einstellungen/dirk/Eigene Dateien/Adoplix/Test/Configuration/testxml.xml";
-            
-            ServerConfiguration serverConf = new ServerConfiguration(fileName);
-            System.out.println( serverConf.getServerId ());
-            System.out.println( serverConf.getPwd ());
-            System.out.println( serverConf.getIntervalGetProjectSec ());
-            System.out.println( serverConf.getMaxClientThreads ());
-            System.out.println( serverConf.getPathTaskConfiguration ());
-            System.out.println( serverConf.getPortExternal ());
-            System.out.println( serverConf.getPortService ());
-            System.out.println( serverConf.getPortLocal ());
-            System.out.println( serverConf.getTimeoutClientMillis ());
-            System.out.println( serverConf.getValidServerId (0));
-            System.out.println( serverConf.getValidServerId (1));
-//            validServerId = serverConf.getValidServerServerId (2);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+    	parseArguments(args);
+    	AdoplixServer server = new AdoplixServer(args);
+    	while (null != server) {
+    		try {
+    			Thread.sleep( 1000 );
+    		}
+    		catch (Exception ex) {}
+    	}
     }
     
     /**
-     * Creates a new instance of AdoplixServer 
+     * Creates a new instance of AdoplixServer
+     * Reads the own configuration and the task-configuration 
+     * @param args Arguments used when program was called
      */
-    public AdoplixServer () {
+    public AdoplixServer (String[] args) {
+    	String taskConfiguration = "";
+    	System.out.println("adoplix");
+    	System.out.flush();
+        long timeToReadConfiguration = System.currentTimeMillis();
+    	_serverConfiguration = new ServerConfiguration(_pathConfiguration);
+        timeToReadConfiguration = System.currentTimeMillis() - timeToReadConfiguration;
+        System.out.println(timeToReadConfiguration);
+    	taskConfiguration = _serverConfiguration.getPathTaskConfiguration();
+    	if (null != taskConfiguration &&
+    			taskConfiguration.length() > 0) {
+    		
+    		_taskConfiguration = new TaskConfiguration(taskConfiguration);
+    	}
+
+        System.exit(0);
     }
     
+    /**
+     * Analyse of Calling Arguments
+     * @param args Arguments used when program was called
+     */
+    private static void parseArguments(String[] args) {
+    	if (args.length > 0) {
+    		args[0] = args[0].toUpperCase();
+    	}
+    	if (args[0].indexOf("H") > 0) {
+    		showArguments();
+    	}
+    	if (args[0].indexOf("F") > 0) {
+    		if (args.length > 1) {
+    			_pathConfiguration = args[1];
+    		}
+    		else {
+    			System.out.println("Parameter F: Dateiangabe fehlt");
+    			System.exit(1);
+    		}
+    	}
+    }
+    
+    /**
+     * Shows used arguments when program was started
+     */
+    private static void showArguments() {
+    	
+    }
 }
